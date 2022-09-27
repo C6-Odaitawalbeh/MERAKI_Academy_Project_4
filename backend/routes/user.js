7ce313bea127bfd458c6d2dee7ddd319df949f9f
Odaitawalbeh <odai.mo.tawalbeh@gmail.com>
2022-09-27 04:05:26 +0300

    login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,7 @@ const authorization = require('../middlewaers/authorization');
 userRouter.post('/register', createUser);
 userRouter.get('/', getAllUsers);
 userRouter.get('/:id', getUserById);
-userRouter.put('/profile/:id', authentication, editProfile);
-userRouter.delete('/profile/:id', authentication, deleteProfile);
+userRouter.put('/profile/:id', authentication, authorization("UPDATE_PROFILE") ,editProfile);
+userRouter.delete('/profile/:id', authentication, authorization("DELETE_PRODUCT") ,deleteProfile);
+
 module.exports = userRouter;
---
+++ frontend/src/components/contexts/login.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export const loginContext = createContext();
+
+const LoginProvider = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+    const [message, setMeesage] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const history = useNavigate();
+
+    useEffect(() => {
+        saveToken(localStorage.getItem('token'));
+    },[])
+
+    const saveToken = (token) => {
+        setToken(token);
+        localStorage.setItem('token', token);
+    }
+
+    const login = async () => {
+        try {
+            const result = await axios.post('http://localhost:5000/user/login', {email,password});
+
+            saveToken(result.data.token);
+
+            history('/'); // main page
+
+            setIsLoggedIn(true);
+
+        } catch (err) {
+            setMeesage(err.result.data);
+        }
+    }
+
+    const state = {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        token,
+        setToken,
+        message,
+        setMeesage,
+        isLoggedIn,
+        setIsLoggedIn,
+        login
+    }
+
+    return (
+        <loginContext.Provider value={state}>
+            {props.children}
+        </loginContext.Provider>
+    )
+};
+
+export default LoginProvider;
