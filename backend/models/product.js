4261f8f73f3419b07da7fcb40709ef50e15613ce
Odaitawalbeh <odai.mo.tawalbeh@gmail.com>
2022-10-03 00:40:26 +0300

    create categories in productSchema

--- backend/models/product.js
+++ backend/models/product.js
@@ -12,6 +12,7 @@ const product = new mongoose.Schema(
         userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
         count: {type: Number , default: 1},
         shorttitle: {type: String},
+        categories: {type: String}
     }
 );
 
